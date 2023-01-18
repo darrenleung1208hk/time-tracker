8484d58311ed9c28e2011531b04efb65b8eab194
Darren Leung <leungyatlong1208@gmail.com>
2023-01-19 00:05:13 +0800

    Set up PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+		}),
+	],
 });
