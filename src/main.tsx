3b76f1fdc14e11cd3ba8404feeabb60962a09eb8
Darren Leung <leungyatlong1208@gmail.com>
2023-02-05 22:25:23 +0800

    feat: removed css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "@/themes";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import "@/styles/global.css";
 import "@fontsource/poppins";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
