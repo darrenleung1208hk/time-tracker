218dcc7b40faae322972cd0291e96ca00b7cd533
Darren Leung <leungyatlong1208@gmail.com>
2023-02-11 16:59:07 +0800

    feat(TT-21): firebase initialization

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase
+const firebase = initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+});
+
+export default firebase;
