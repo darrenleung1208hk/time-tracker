d83eb7f0a79ed0dd9e15e09da66794e1ee4aa2a2
Darren Leung <leungyatlong1208@gmail.com>
2023-02-12 21:37:36 +0800

    feat(TT-19): sign in page basic UI

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import { signInGoogle } from "@/lib/auth";
 
 const SignIn = () => {
   return (
-    <Button variant={"contained"} onClick={signInGoogle}>
-      Sign in with Google
-    </Button>
+    <Stack
+      justifyContent={"center"}
+      alignItems={"center"}
+      sx={{ height: "80vh" }}
+    >
+      <Typography variant={"h4"} fontWeight={700} paragraph>
+        Time Tracker
+      </Typography>
+      <Typography fontWeight={700} paragraph>
+        Help you to manage your time.
+      </Typography>
+      <Button variant={"contained"} onClick={signInGoogle}>
+        Sign in with Google
+      </Button>
+    </Stack>
   );
 };
 
