596d4df9068c926925d713a1ce7f1cc6582f9d18
Darren Leung <leungyatlong1208@gmail.com>
2023-02-12 17:16:37 +0800

    feat(TT-19): added sign in button to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import React, { useState } from "react";
-import reactLogo from "@/assets/react.svg";
+import React from "react";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import { signInGoogle } from "@/lib/auth";
 import Layout from "@/layout/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Layout>
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <Paper variant={"outlined"} sx={{ p: 4, textAlign: "center" }}>
+        <Button variant={"contained"} onClick={signInGoogle}>
+          Sign in with Google
+        </Button>
+      </Paper>
     </Layout>
   );
 }
