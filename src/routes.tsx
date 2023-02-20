0f67727a523b3699160c31bf54ced85a1caf36cd
Darren Leung <leungyatlong1208@gmail.com>
2023-02-21 00:27:38 +0800

    feat(TT-19): redirect from index route to login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { RouteObject } from "react-router-dom";
+import { redirect, RouteObject } from "react-router-dom";
 import { Home, Login } from "@/pages";
 
 export const ROUTES = {
+  INDEX: "/",
   LOGIN: "/login",
   HOME: "/browse",
 };
 
 const routes: RouteObject[] = [
+  {
+    path: ROUTES.INDEX,
+    loader: async () => {
+      return redirect(ROUTES.LOGIN);
+    },
+  },
   {
     path: ROUTES.LOGIN,
     element: <Login />,
