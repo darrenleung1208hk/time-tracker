bfd1b5b6a690c4f82501970b286bad3b2ba02f4e
Darren Leung <leungyatlong1208@gmail.com>
2023-02-25 13:50:23 +0800

    feat(TT-7): fetch docs with filter

--- src/lib/firestore/utils.ts
+++ src/lib/firestore/utils.ts
@@ -1,17 +1,34 @@
 import {
   collection,
   DocumentData,
+  FieldPath,
   getDocs,
   getFirestore,
+  query,
+  where,
+  WhereFilterOp,
 } from "firebase/firestore";
 import app from "@/lib/firebase";
 
 const db = getFirestore(app);
 
-export async function fetchDocuments(path: string): Promise<DocumentData[]> {
+export async function fetchDocuments(
+  path: string,
+  condition?: {
+    fieldPath: string | FieldPath;
+    opStr: WhereFilterOp;
+    value: unknown;
+  },
+): Promise<DocumentData[]> {
   try {
     const data: DocumentData[] = [];
-    const querySnapshot = await getDocs(collection(db, path));
+    const q = condition
+      ? query(
+          collection(db, path),
+          where(condition.fieldPath, condition.opStr, condition.value),
+        )
+      : query(collection(db, path));
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       data.push(doc.data());
     });
