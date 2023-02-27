23ee6ec1c69654ee28851e4a1bdfb0b0cdbd2913
Darren Leung <leungyatlong1208@gmail.com>
2023-02-28 00:12:51 +0800

    feat(TT-7): render entries returned from firestore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Entry from "@/components/Entry/Entry";
 import PrivateRoute from "@/components/PrivateRoute";
 import Layout from "@/layout/Layout";
+import { useEntries } from "@/lib/entry/hooks";
+import { COLOR } from "@/themes/palette";
 
 export const Home = () => {
+  const { data: entries, isLoading } = useEntries();
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <PrivateRoute>
-      <Layout>hi</Layout>
+      <Layout>
+        <Stack>
+          {entries.map((e) => (
+            <Entry
+              key={e.id}
+              category={"Task"}
+              title={e.title}
+              color={COLOR.green400}
+              duration={1}
+            />
+          ))}
+        </Stack>
+      </Layout>
     </PrivateRoute>
   );
 };
