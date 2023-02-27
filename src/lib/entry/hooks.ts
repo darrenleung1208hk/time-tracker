f01b4a96fdf6009de6c1527fb81b0f3545bc1a4e
Darren Leung <leungyatlong1208@gmail.com>
2023-02-27 23:55:46 +0800

    feat(TT-7): useEntries hook

---
+++ src/lib/entry/hooks.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "@/lib/auth";
+import { fetchEntries } from "@/lib/entry/api";
+import { Entry } from "@/lib/entry/types";
+
+export const useEntries = () => {
+  const { user } = useAuth();
+  const [data, setData] = useState<Entry[]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+
+  const isError = Boolean(error);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (user) {
+          const res = await fetchEntries(user?.uid);
+          setData(res);
+        }
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [user]);
+
+  return {
+    data,
+    error,
+    isLoading,
+    isError,
+  };
+};
